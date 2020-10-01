ef1b62f062d119d203ebea54838d506dea770ba5
GURU PRASAD SHAMANNA <gshamann@in.ibm.com>
2020-10-01 16:58:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,1 @@
-provider "ibm" {
-  // ibmcloud_api_key   = "${var.ibmcloud_api_key}"
-   iam_token = "xx"
-   iam_refresh_token = "yy"
-}
 
-terraform {
-  required_providers {
-    ibm = {
-      version = "= 1.7"
-    }
-  }
-}
