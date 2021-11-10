2f7a9e43ef32e766bc2a0df537d37dc3335a61c7
nishubharti <32607124+nishubharti@users.noreply.github.com>
2021-11-10 19:30:11 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,53 +1,53 @@
 # Configure these variables
 
-variable "sample_var" {
-  description = "A sample_var to pass to the template."
-  default     = "hello"
-}
+# variable "sample_var" {
+#   description = "A sample_var to pass to the template."
+#   default     = "hello"
+# }
 
-variable "Network_type" {
-  description = "Type of network"
-}
+# variable "Network_type" {
+#   description = "Type of network"
+# }
 
-variable "sensitive_var" {
-  description = "A sensitive var to pass to the template."
-  default     = "I am Sensitivie. Make me secure"  
-}
+# variable "sensitive_var" {
+#   description = "A sensitive var to pass to the template."
+#   default     = "I am Sensitivie. Make me secure"  
+# }
 
-variable "sleepy_time" {
-  description = "How long our local-exec will take a nap."
-  default     = 0
-}
+# variable "sleepy_time" {
+#   description = "How long our local-exec will take a nap."
+#   default     = 0
+# }
 
-variable "TF_VERSION" {
-  description = "terraform version."
-  default     = 0.13
-}
+# variable "TF_VERSION" {
+#   description = "terraform version."
+#   default     = 0.13
+# }
 
-variable "aws_access_key" {
-  description  = "AWS access key"
-  type         = string
-}
+# variable "aws_access_key" {
+#   description  = "AWS access key"
+#   type         = string
+# }
 
-variable "aws_secret_key" {
-  description  = "AWS secret key"
-  type         = string
-}
+# variable "aws_secret_key" {
+#   description  = "AWS secret key"
+#   type         = string
+# }
 
-variable "aws_region" {
-  description  = "AWS region"
-  type         = string
-  default      = "us-east-1"
-}
+# variable "aws_region" {
+#   description  = "AWS region"
+#   type         = string
+#   default      = "us-east-1"
+# }
 
-variable "image_id" {
-  type        = string
-  description = "The id of the machine image (AMI) to use for the server."
-  # default = "afasdf"
+# variable "image_id" {
+#   type        = string
+#   description = "The id of the machine image (AMI) to use for the server."
+#   # default = "afasdf"
 
-  validation {
-    condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
-    error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
-  }
-}
+#   validation {
+#     condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+#     error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
+#   }
+# }
 
